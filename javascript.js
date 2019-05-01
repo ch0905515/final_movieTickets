b5dba4b00bfb4e23fb77d85479df510f2716677d
ch0905515 <47401683+ch0905515@users.noreply.github.com>
2019-05-01 16:05:39 -0500

    Adding varArr
    adding arrays for the changing variables

--- javascript.js
+++ javascript.js
@@ -1,16 +1,29 @@
 
-
+var mainCount = 3;
 
 $.getJSON("https://api.themoviedb.org/3/movie/now_playing?api_key=54400abc068adca7102fee24733fbc06&language=en-US&page=1", function (jsObject) {
     
    console.log(jsObject);
 
+   function createVarArr(x){
+       var varArr= [];
+
+       for(var p = 0; p < x; p++){
+        varArr.push([0,0]);
+       }
+
+       console.log(varArr);
+       return varArr;
    
+   }
+
+
     var app = new Vue({
         el: '#app',
         data: {
-            count: 3,
-            arr:  jsObject.results
+            count: mainCount,
+            arr:  jsObject.results,
+            varArr: createVarArr(mainCount)
         },
         methods: {
             getImage: function(i){
